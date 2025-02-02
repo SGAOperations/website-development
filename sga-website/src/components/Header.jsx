351abc6170e966f531304595ef9dce1bac638f15
Audrey Ng <audrey518@gmail.com>
2025-02-02 18:32:14 -0500

    Updated landing page and fixed positioning

--- sga-website/src/components/Header.jsx
+++ sga-website/src/components/Header.jsx
@@ -4,29 +4,29 @@ import HeaderDropdowns from './HeaderDropdowns';
 
 export default function Header() {
   return (
-    <header 
-      className="flex flex-col justify-center items-center h-auto w-full fixed top-0 left-0 p-5" 
-      style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
-    >
-      <div className="flex items-center">
-        <SplitText
-          text="SGA"
-          className="text-9xl font-bold text-sga-red text-center"
-          delay={100}
-          animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
-          animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-          easing="easeOutCubic"
-          threshold={0.1}
-          rootMargin="0px"
-        /> 
-        <div
-          class="inline-block h-[180px] min-h-[1em] w-1 self-stretch bg-dark dark:bg-black mx-8">
+    <div className="relative w-screen">
+      <header 
+        className="flex flex-col justify-center items-center h-auto w-screen relative top-0 p-5" 
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
+        <div className="flex items-center">
+          <SplitText
+            text="SGA"
+            className="text-9xl font-bold text-sga-red text-center"
+            delay={100}
+            animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            easing="easeOutCubic"
+            threshold={0.1}
+            rootMargin="0px"
+          /> 
+          <div className="inline-block h-[180px] min-h-[1em] w-1 self-stretch bg-dark dark:bg-black mx-8"></div>
+          <img src={sgaLogo} alt="SGA Logo" className="w-52 h-auto" />
         </div>
-        <img src={sgaLogo} alt="SGA Logo" className="w-52 h-auto" />
-      </div>
-      <div className="mt-4">
-        <HeaderDropdowns />
-      </div>
-    </header>
-  )
+        <div className="mt-4">
+          <HeaderDropdowns />
+        </div>
+      </header>
+    </div>
+  );
 }
