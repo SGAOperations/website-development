e5faff03c6c90a96829d930adf62da2f2c1f0ba7
wlenig <30681316+wlenig@users.noreply.github.com>
2025-10-22 15:58:20 -0400

    Add rudimentary multi-line handling of text in Paragraph component

--- sga-puck/components/puck/Paragraph.tsx
+++ sga-puck/components/puck/Paragraph.tsx
@@ -5,9 +5,12 @@ export interface ParagraphProps {
 export const Paragraph: React.FC<ParagraphProps> = ({
     text
 }) => {
-    return (
-        <p>
-            {text}
+    // TODO: robust rich text handling
+    const paragraphs = text.split('\n').map((line, index) => (
+        <p key={index} className="mb-4 last:mb-0">
+            {line}
         </p>
-    )
+    ))
+
+    return <>{paragraphs}</>
 }
