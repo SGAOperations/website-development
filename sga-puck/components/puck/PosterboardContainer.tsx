7da3d8cbb754651dab3a525fbd1621570b65b74a
wlenig <30681316+wlenig@users.noreply.github.com>
2025-10-22 17:39:14 -0400

    Posterboard now tiles children masonry-style, applies gap as margin between vertical children

--- sga-puck/components/puck/PosterboardContainer.tsx
+++ sga-puck/components/puck/PosterboardContainer.tsx
@@ -8,6 +8,10 @@ export interface PosterboardContainerProps {
 export const PosterboardContainer: React.FC<PosterboardContainerProps> = ({
     content: Content,
     gap = "",
-}) => (
-    <Content className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${gap}`} />
-)
+}) => {
+    // TODO: robust handling of gap to margin conversion
+    // make the prop and setting numerical
+    const bottomMargin = gap.includes("gap-") ? gap.replace("gap-", "mb-") : ""
+    // NOTE: [&>*]:style applies style to direct children
+    return <Content className={`columns-1 md:columns-2 lg:columns-3 ${gap} [&>*]:break-inside-avoid [&>*]:${bottomMargin}`} />
+}
