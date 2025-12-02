c4afc71cf4a93b6cb25ef9b814a1917aaa28f062
wlenig <30681316+wlenig@users.noreply.github.com>
2025-12-02 18:38:56 -0500

    Update Paragraph component to use `richtext` field type

--- sga-puck/components/puck/Paragraph.tsx
+++ sga-puck/components/puck/Paragraph.tsx
@@ -4,25 +4,27 @@ export interface ParagraphProps {
     text: string;
 }
 
-export const Paragraph: React.FC<ParagraphProps> = ({
-    text
-}) => {
-    // TODO: robust rich text handling
-    const paragraphs = text.split('\n').map((line, index) => (
-        <p key={index} className="mb-2 last:mb-0 break-words">
-            {line}
-        </p>
-    ))
+// export const Paragraph: React.FC<ParagraphProps> = ({
+//     text
+// }) => {
+//     // TODO: robust rich text handling
+//     const paragraphs = text.split('\n').map((line, index) => (
+//         <p key={index} className="mb-2 last:mb-0 break-words">
+//             {line}
+//         </p>
+//     ))
 
-    return <>{paragraphs}</>
-}
+//     return <>{paragraphs}</>
+// }
 
 export const ParagraphConfig: ComponentConfig<ParagraphProps> = {
     fields: {
-        text: { type: "textarea" },
+        // text: { type: "textarea" },
+        text: { type: "richtext" },
     },
     defaultProps: {
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
-    render: (props) => <Paragraph {...props} />,
+    // render: (props) => <Paragraph {...props} />,
+    render: ({ text }) => text,
 }
