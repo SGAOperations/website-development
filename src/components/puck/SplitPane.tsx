19c1168f3c2d3cd4862593bd02a0cc99e10012a9
Grace Silge <silge.g@northeastern.edu>
2026-03-01 23:21:51 -0500

    WEB-46 hacky solution, needs mobile and vertical work

--- src/components/puck/SplitPane.tsx
+++ src/components/puck/SplitPane.tsx
@@ -17,19 +17,25 @@ export interface SplitPanePropsForRender extends SplitPaneProps {
 export const SplitPane: React.FC<SplitPanePropsForRender> = ({
     paneOneContent: ContentOne,
     paneTwoContent: ContentTwo,
-    direction = "flex-row",
+    direction = "col",
     paneOneRatio = 1,
-    paneTwoRatio = 2,
+    paneTwoRatio = 4,
     gap = "gap-4",
     puck
 }) => {
     // TODO: robust handling of gap to margin conversion
     // make the prop and setting numerical
 
+    const tailwindHacks = "grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11 grid-cols-12 " +
+        "grid-rows-1 grid-rows-2 grid-rows-3 grid-rows-4 grid-rows-5 grid-rows-6 grid-rows-7 grid-rows-8 grid-rows-9 grid-rows-10 grid-rows-11 grid-rows-12 " +
+        "col-start-1 col-start-2 col-start-3 col-start-4 col-start-5 col-start-6 col-start-7 col-start-8 col-start-9 col-start-10 col-start-11 col-start-12 " +
+        "col-end-1 col-end-2 col-end-3 col-end-4 col-end-5 col-end-6 col-end-7 col-end-8 col-end-9 col-end-10 col-end-11 col-end-12 " +
+        "row-start-1 row-start_2 row-start_3 row-start_4 row-start_5 row-start_6 row-start_7 row-start_8 row-start_9 row-start_10 row-start_11 row-start_12 " +
+        "row-end-1 row-end-2 row-end-3 row-end-4 row-end-5 row-end-6 row-end-7 row-end-8 row-end-9 row-end-10 row-end-11 row-end-12"
     const totalSize = paneOneRatio + paneTwoRatio
-    return <div ref={puck.dragRef} className={`grid grid-cols-1 md:grid-${direction}s-${totalSize} ${gap}`}>
-            <ContentOne className={`md:${direction}-start-1 md:col-end-${paneOneRatio + 1}`}/>
-            <ContentTwo className={`md:${direction}-start-${paneOneRatio + 1} md:col-end-${totalSize + 1}`} />
+    return <div ref={puck.dragRef} className={`grid grid-${direction}s-${totalSize} ${gap}`}>
+            <ContentOne className={`${direction}-start-1 ${direction}-end-${paneOneRatio + 1}`}/>
+            <ContentTwo className={`${direction}-start-${paneOneRatio + 1} ${direction}-end-${totalSize + 1}`} />
             </div>
 }
 export const SplitPaneConfig: ComponentConfig<SplitPaneProps> = {
@@ -59,7 +65,7 @@ export const SplitPaneConfig: ComponentConfig<SplitPaneProps> = {
         paneOneContent: null,
         paneTwoContent: null,
         gap: "gap-4",
-        direction: "flex-row",
+        direction: "col",
         paneOneRatio: 1,
         paneTwoRatio: 4
     },
