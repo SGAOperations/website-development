cacd23dcd54360c67123cdf33e6095da8842a7fc
wlenig <30681316+wlenig@users.noreply.github.com>
2026-03-19 05:45:19 -0400

    WEB-96 Upgrade to Nextjs 16, React 19

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   return NextResponse.next();
 }
